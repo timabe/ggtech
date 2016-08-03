73b86d0227ce70299a5536a2166c628b2902cd5e
Tim Abraham <timmy4@gmail.com>
2016-08-03 10:44:14 -0700

    Update scale_fill_tech.R
    adding ando color palette

--- R/scale_fill_tech.R
+++ R/scale_fill_tech.R
@@ -15,7 +15,8 @@ scale_fill_tech <- function(theme="airbnb", tech_key = list(
                   facebook = c("#3b5998", "#6d84b4", "#afbdd4", "#d8dfea"),
                   google = c("#5380E4", "#E12A3C", "#FFBF03", "#00B723"),
                   etsy = c("#F14000", "#67B6C3", "#F0DA47", "#EBEBE6", "#D0D0CB"),
-                  twitter = c("#55ACEE", "#292f33", "#8899a6", "#e1e8ed")
+                  twitter = c("#55ACEE", "#292f33", "#8899a6", "#e1e8ed"),
+                  ando = c("#D9AC7F", "#F7323F", "000000", "FFFFFF","#E51933","#444444", "#F2F2F2")
                     )) {
   
   scale_fill_manual(values=tech_key[[theme]])
